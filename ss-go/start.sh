c9bfcfb28afce3cc70b531f4cb59d6e6fd773797
eagle <eagle@localhost.localdomain>
2016-10-24 20:41:53 -0500

    路径问题

--- ss-go/start.sh
+++ ss-go/start.sh
@@ -9,6 +9,7 @@ fi
 fullpath="$(pwd)/$0"
 #cd 到本脚本所在的目录
 cd $(dirname $fullpath)
+cd core
 ./shadowsocks-server-linux64-1.1.5 >/dev/null 2>&1 &
 if ps aux | grep -v grep | grep -q shadowsocks-server-linux64;then
     echo "shadowsocks-server-linux64 has started"
