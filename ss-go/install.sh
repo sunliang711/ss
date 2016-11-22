421856367091659b9bf419fb11783516951b110f
sunliang <sunliang711@163.com>
2016-11-22 18:06:52 +0800

    增加了防火墙的设置

--- ss-go/install.sh
+++ ss-go/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if (($EUID!=0));then
     echo "Need root priviledge!" >&2
     exit 1
@@ -13,26 +12,31 @@ fi
 
 
 fullpath="$PWD/$0"
-echo "fullpath:$fullpath"
 #get this file's path
 realpath=$(dirname "$fullpath")
-echo "realpath:$realpath"
 
 #install shadowsocks
 mkdir -pv "$ROOT"
 cd "$realpath"
 cp core/shadowsocks-server-linux64* "$ROOT"
 cp core/config.json "$ROOT"
-cp ss-go "$ROOT"
+cp core/otherRule "$ROOT"
+cp core/ss-go "$ROOT"
 
 #auto start on boot
-if ! grep -q "ss-go start" /etc/rc.local;then
-    sed -i "/^exit 0/i$ROOT/ss-go start" /etc/rc.local
+if [ ! -f /etc/rc.local ];then
+    echo "***Warning***Cannot found /etc/rc.local,so cannot start ss-go on startup">&2
+    echo "***Warning***You can add: $ROOT/ss-go start to startup script manually!">&2
+else
+    if ! grep -q "ss-go start" /etc/rc.local;then
+        sed -i "/^exit 0/i$ROOT/ss-go start" /etc/rc.local
+    fi
 fi
 
 #add alias for ss-go
 if ! grep -q "alias ss-go" ~/.bashrc;then
     echo "alias ss-go=\"$ROOT/ss-go\"">>~/.bashrc
+    echo "***Info***Source ~/.bashrc to use alias 'ss-go'"
 fi
 
 #start server
