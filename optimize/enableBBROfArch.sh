5c81044feb4e13fb01442e5e01afb9997f4a6c65
sunliang <sunliang711@163.com>
2017-02-16 14:10:52 +0800

    better

--- optimize/enableBBROfArch.sh
+++ optimize/enableBBROfArch.sh
@@ -8,3 +8,6 @@ fi
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
+
+#echo "net.ipv4.tcp_congestion_control=bbr" >  /etc/sysctl.d/80-bbr.conf
+
