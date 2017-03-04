26a0dba2e4fb774e6e6b76d14600eb15c498b6f0
sunliang <sunliang711@163.com>
2017-03-04 18:29:14 +0800

    archlinux在docker上启动WordPress

--- lamp/wordpressOfArch/createContainer.sh
+++ lamp/wordpressOfArch/createContainer.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-
-#pull mysql
-docker pull mysql
-
-#pull wordpress
-docker pull sunliang711/wordpress
-
-#pull data
-docker pull sunliang711/data
 
 #start data
 docker run -d --name data-one sunliang711/data tail -f /dev/null
