62a572c2bc0fd518f7176aa8a502229121aa82d0
sunliang <sunliang711@163.com>
2017-03-04 17:29:56 +0800

    archlinux bbr enable

--- optimize/enableBBROfArch.sh
+++ optimize/enableBBROfArch.sh
@@ -5,9 +5,13 @@ if (($EUID!=0));then
     exit 1
 fi
 
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
+#echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+#echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 
-#echo "net.ipv4.tcp_congestion_control=bbr" >  /etc/sysctl.d/80-bbr.conf
+if command -v pacman >/dev/null 2>&1;then
+    echo "net.ipv4.tcp_congestion_control=bbr" >  /etc/sysctl.d/80-bbr.conf
+    echo "please reboot now" 
+else
+	echo -e "Only support $(tput setaf 1)Archlinux$(tput sgr0) now,because bbr need linux kernel 4.9!"
+fi
 
