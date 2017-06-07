9e03acaea1b9df575034c9aa879ae8e78ca4c4a9
sunliang <sunliang711@163.com>
2017-06-07 14:29:51 +0800

    解决wordpress只有主页能打开，其他页面打不开的问题

--- lamp/debian8.8lamp.sh
+++ lamp/debian8.8lamp.sh
@@ -14,3 +14,10 @@ sed -ri 's=([ ]+)//(.+AllowNoPassword.+)=\1\2=' /etc/phpmyadmin/config.inc.php
 
 echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
 service apache2 restart
+
+#如果运行wordpress的时候主页可以打开，其他页面打不开
+#在wordpress后台-设置-固定链接 使用自定义结构的时候会出现，把固定链接设置成『默认』方式即可
+#或者如果非要用自定义结构的话，则有两个地方要设置
+#1.要启用rewrite模块，通过a2enmod rewrite命令
+#2.在/etc/apache2/apache2.conf中
+#  把<Directory /var/www/>里面的AllowOverride None改成AllowOverride All
