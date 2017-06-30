67d0af640e2cd25f3d4b028363bcdebf9beb3f57
sunliang711 <sunliang711@163.com>
2017-06-30 12:03:25 +0800

    add installation script for frps on debian

---
+++ frp/installFrpsOnDebian8.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#stop service if needed
+systemctl stop frps.service >/dev/null 2>&1
+
+root=/opt/frps
+if [ -d "$root" ];then
+    rm -rf "$root"
+fi
+mkdir -p "$root"
+
+pidfiledir=/var/run/frp
+if [ ! -d "$pidfiledir" ];then
+    mkdir "$pidfiledir"
+fi
+
+#exe and config file
+cp -r ./frp-bin-linux64/* "$root"
+
+#service file
+cat > /lib/systemd/system/frps.service <<EOF
+[Unit]
+Description=frps
+After=network.target
+
+[Service]
+Type=forking
+User=root
+PIDFile=$pidfiledir/frps.pid
+ExecStart=$root/frps -c $root/frps.ini
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#start service
+systemctl start frps.service
+
+#enable service
+systemctl enable frps.service
