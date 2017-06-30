8160ced9b14740c994454f7bd8191f7efc0ce5b0
sunliang711 <sunliang711@163.com>
2017-06-30 10:30:19 +0800

    fix lib path

--- ss-libev-install/installMultiPortShadowsockslibev.sh
+++ ss-libev-install/installMultiPortShadowsockslibev.sh
@@ -32,7 +32,7 @@ cp ./ss-libev.service /lib/systemd/system
 cp ./ss-multi-port.sh "$root"
 
 #shared library
-cp ./libmbedcrypto.so.0 /usr/local/lib/libmbedcrypto.so.0
+cp ./libmbedcrypto.so.0 /usr/lib/libmbedcrypto.so.0
 cp ./libsodium.so.18.1.1 /usr/lib/libsodium.so.18
 
 systemctl daemon-reload
