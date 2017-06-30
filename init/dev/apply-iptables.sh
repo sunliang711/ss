e22dde754f3930c6f7ad06b76bb3b6d5bfee5f2a
sunliang <sunliang711@163.com>
2017-06-30 18:51:10 +0800

    add ss-libev port automaticlly

--- init/dev/apply-iptables.sh
+++ init/dev/apply-iptables.sh
@@ -2,5 +2,11 @@
 basic=/opt/iptables/iptables.rules
 other=/opt/iptables/other.rules
 end=/opt/iptables/end.rules
+all=/opt/iptables/all.rules
 
-cat $basic $other $end | /sbin/iptables-restore
+cat $basic >$all
+cat $other >>$all
+/opt/iptables/ss-libev-rules>>$all
+cat $end >>$all
+
+/sbin/iptables-restore $all
